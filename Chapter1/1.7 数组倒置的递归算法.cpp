fddb576851d36b43721bac85d71401189f2f8d6c
DraganZHANG <15779677775@163.com>
2017-12-30 23:41:48 +0800

    数组倒置添加了迭代版

--- Chapter1/1.7 数组倒置的递归算法.cpp
+++ Chapter1/1.7 数组倒置的递归算法.cpp
@@ -16,9 +16,23 @@ void reverse(int *A,int low ,int high){//数组倒置(多递归基递归版) 复杂度:O(logN)
 	 
 }
 
+void reverse1(int *A,int low ,int high){//数组倒置(直接改造而得的迭代版) 
+	next://算法起始位置添加跳转标志 
+		if(low<high){
+			swap(A[low],A[high]);
+			low++; high--;
+			goto next;//跳转至算法体的起始位置,迭代地倒置A(low,high) 
+		} 
+	
+}
 int main(){
 	int A[7]={1,2,3,4,5,6,7};
 	reverse(A,0,6);
+	for(int i=0;i<7;i++){
+		cout<<A[i]<<" ";
+	}
+	cout<<endl;
+		reverse1(A,0,6);
 	for(int i=0;i<7;i++){
 		cout<<A[i]<<" ";
 	}
