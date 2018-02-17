9ce976d80d45928928b6ee3cede35592a59c0d9e
DraganZHANG <15779677775@163.com>
2018-02-17 15:06:12 +0800

    update

--- Stack/数制转换/源.cpp
+++ Stack/数制转换/源.cpp
@@ -5,12 +5,12 @@ using namespace std;
 void conversion() {
 	//对于任意一个非负十进制整数,打印输出与其等值的八进制数
 	stack<int> S;//构造空栈
-	
+
 	int N;
 
 	cin >> N;
 
-	while(N) {
+	while (N) {
 		S.push(N % 8);
 		N = N / 8;
 	}
@@ -18,7 +18,7 @@ void conversion() {
 	while (!S.empty()) {
 		int e = S.top();
 		cout << e;
-		S.pop();		
+		S.pop();
 	}
 
 }
